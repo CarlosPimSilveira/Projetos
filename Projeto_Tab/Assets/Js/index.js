60b94b1a7b97c5a10c3a9a8de242931ca37900e5
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-12 23:11:25 -0300

    atualizações

--- Projeto_Tab/Assets/Js/index.js
+++ Projeto_Tab/Assets/Js/index.js
@@ -4,9 +4,9 @@ const mainPrincipal = document.querySelector('.main')
 btnCalcular.addEventListener('click', () => {
     const txtNum = document.querySelector('#txtNum').value
     for (let c = 0; c <= 10; c++) {
-        let ResP[c] = document.createElement("p")
+        let ResP = document.createElement("p")
         ResP.classList.add('resposta-tabuada')
         ResP.innerText = c + ' x ' + txtNum + ' = ' + (txtNum * c) 
+        mainPrincipal.appendChild(ResP)
     }
-    mainPrincipal.appendChild(ResP)
 })
