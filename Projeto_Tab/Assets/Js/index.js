2b67e820361bb250f448fccd6c0ba384e9ec9a0b
Carlos Pim <carlospimsilveira@gmail.com>
2026-09-12 23:48:01 -0300

    Update index.js

--- Projeto_Tab/Assets/Js/index.js
+++ Projeto_Tab/Assets/Js/index.js
@@ -1,15 +1,18 @@
 const btnCalcular = document.querySelector('.btnCalcular')
-const mainPrincipal = document.querySelector('.main')
+
+const DivResp = document.createElement('div')
+DivResp.classList.add('DivResp')
+document.body.appendChild(DivResp)
 
 btnCalcular.addEventListener('click', () => {
+
     const txtNum = document.querySelector('#txtNum').value
-    const DivResp = document.createElement('div')
-    DivResp.classList.add('DivResp')
+    DivResp.innerHTML = ''
+
     for (let c = 0; c <= 10; c++) {
-        let ResP = document.createElement("p")
+        let ResP = document.createElement('p')
         ResP.classList.add('resposta-tabuada')
-        ResP.innerText = c + ' x ' + txtNum + ' = ' + (txtNum * c) 
+        ResP.innerText = c + ' x ' + txtNum + ' = ' + (txtNum * c)
         DivResp.appendChild(ResP)
     }
-    document.body.appendChild(DivResp)
 })
